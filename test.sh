ea14df59df2597b0b05f5bc8da5d4b4114e26f66
Jvpl001 <98943394+Jvpl001@users.noreply.github.com>
2025-07-18 11:26:44 +0330

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-cat <<REALEND >/mnt/mnt/chroot.sh
-
-echo "-------------------------------------------------"
-echo "Install Complete, You can reboot now"
-echo "-------------------------------------------------"
-REALEND
-
-arch-chroot /mnt sh /mnt/chroot.sh
