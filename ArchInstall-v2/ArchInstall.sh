8223ad910901b48a87f20847087d47f2a98f9657
Jvpl001 <98943394+Jvpl001@users.noreply.github.com>
2025-07-21 16:07:31 +0330

    Update ArchInstall.sh

--- ArchInstall-v2/ArchInstall.sh
+++ ArchInstall-v2/ArchInstall.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 echo "enter username:"
 read username
+echo "enter the username password:"
+read user_pass
+echo "enter the root password:"
+read root_pass
+lsblk
+echo "enter the installation drive(sdX):"
+read intall_drive
+echo "enter the host name:"
+read host_name
 
-curl https://github.com/Jvpl001/Scripts.git
+user_pass_en=$(mkpasswd -m yescrypt $user_pass)
+root_pass_en=$(mkpasswd -m yescrypt $root_pass)
+
+pacman-key --init
+pacman-key --populate
+
+sed -i 's/^            "username": "username"/            "username": "'$username'"/' user_credentials.json
+sed -i 's/^    "root_enc_password": "rootpass",/    "root_enc_password": "'$root_pass_en'",/' user_credentials.json
+sed -i 's/^            "enc_password": "userpasss",/            "enc_password": "'$user_pass_en'",/' user_credentials.json
+
+sed -i 's/^                "device": "/dev/sda",/                "device": "'$intall_drive'",/' user_configuration.json
+sed -i 's/^    "hostname": "Archmachine",/    "hostname": "'$host_name'",/' user_configuration.json
+
+cat user_credentials.json
