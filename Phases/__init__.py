2526e7c2bab800376bef4bb677a07f285e245241
Jvpl001 <98943394+Jvpl001@users.noreply.github.com>
2025-09-16 06:46:05 +0000

    Delete Phases directory

--- Phases/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Package initializer for installation phases
-# Expose key functions for convenience imports
-
-from .base_install import base_install, chroot_config
-from .fdisk_setup import list_disks, run_fdisk
-from .liberary import run_command, confirm, write_file
-from .check_requirements import checkUEFI, require_root, ensure_dependencies
-from .user_input import get_install_config, InstallConfig
